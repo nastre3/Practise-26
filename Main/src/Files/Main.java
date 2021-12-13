ccbfc89fdc703f0c24e78906aa4c0994426c18b8
Anastasia <nastre70@gmail.com>
2021-12-13 18:50:00 +1000

    Finished 2nd video about working with files

--- Main/src/Files/Main.java
+++ Main/src/Files/Main.java
@@ -11,11 +11,35 @@ public class Main {
         boolean isExists = file.exists();
         System.out.println(isExists);
         System.out.println(isCreated);
-         */
+
 
         File file = new File("Main/src/Files/Test2.txt");
         boolean isExist = file.exists();
         System.out.println(isExist);
+*/
+        // проверка, что директория есть
+        File file = new File("Main/src/Files");
+        boolean isDir = file.isDirectory();
+        System.out.println(isDir);
 
+        // создание директории
+        File file2 = new File("Main/src/Files/Test3");
+        boolean isMake = file2.mkdir();
+        System.out.println(isMake);
+
+        // удаление директории (только пустой)
+        boolean isDelete = file2.delete();
+        System.out.println(isDelete);
+
+        // удаление непустой директории невозможно
+        File file3 = new File("Main/src/TestDir");
+        boolean isDelete2 = file3.delete();
+        System.out.println(isDelete2);
+
+        // получение названий файлов в директории
+        String[] files = file3.list();
+        for (String fileName: files) {
+            System.out.println(fileName);
+        }
     }
 }
