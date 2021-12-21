e8ffbe4f75b2e7469abd6ceb45654510f0216e8d
Anastasia <nastre70@gmail.com>
2021-12-21 20:32:07 +1000

    Fixed IOExceptionExample

--- Main/src/exeptions/IOExceptionExample.java
+++ Main/src/exeptions/IOExceptionExample.java
@@ -2,27 +2,38 @@ package exeptions;
 
 import java.io.*;
 
+class ConvertEx extends Exception {
+    ConvertEx(String message) {
+        super(message);
+    }
+}
+
 public class IOExceptionExample {
     /* Никаких throws и связанных с ним падений */
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
        /* можно указывать все что угодно,
           если не получится, программа вежливо расскажет,
           что именно пошло не так
         */
-        if (copyFileUsingStream("C:\\Users\\Anastasia\\IdeaProjects\\Practise-26\\Main\\src\\exeptions\\utf-8.txt",
-                "utf-8",
-                "C:\\Users\\Anastasia\\IdeaProjects\\Practise-26\\Main\\src\\exeptions\\win1251.txt",
-                "windows-1251")){
+        try {
+            copyFileUsingStream("C:\\Users\\Anastasia\\IdeaProjects\\Practise-26\\Main\\src\\exeptions\\utf-8.txt",
+                    "utf-8",
+                    "C:\\Users\\Anastasia\\IdeaProjects\\Practise-26\\Main\\src\\exeptions\\win1251.txt",
+                    "windows-1251");
             System.out.println("Перекодировка прошла удачно!");
+        } catch (ConvertEx e) {
+            // e.printStackTrace();
+            System.out.println(e.getMessage());
         }
-
     }
 
-    private static boolean copyFileUsingStream(String sourceFilename, String sourceEnc,
-                                               String destFilename, String descEnc) {
 
-        try (Reader fis = new InputStreamReader(new FileInputStream(new File(sourceFilename)), sourceEnc);
-             Writer fos = new OutputStreamWriter(new FileOutputStream(new File(sourceFilename)), sourceEnc)) {
+    // если исключения не выбрасываются, то возвращает boolean, иначе - void
+    private static void copyFileUsingStream(String sourceFilename, String sourceEnc,
+                                            String destFilename, String descEnc) throws ConvertEx {
+
+        try (Reader fis = new InputStreamReader(new FileInputStream(sourceFilename), sourceEnc);
+             Writer fos = new OutputStreamWriter(new FileOutputStream(destFilename), descEnc)) {
             /* все открылось, можно копировать */
 
             char[] buffer = new char[1024];
@@ -30,19 +41,12 @@ public class IOExceptionExample {
             while ((length = fis.read(buffer)) > 0) {
                 fos.write(buffer, 0, length);
             }
+        } catch (FileNotFoundException ex) {
+            throw new ConvertEx("Не удалось открыть файл " + ex.getMessage());
+        } catch (UnsupportedEncodingException ex) {
+            throw new ConvertEx("Указана неизвестная кодировка " + ex.getMessage());
+        } catch (IOException ex) {
+            throw new ConvertEx("Возникла ошибка при копировании");
         }
-        catch (FileNotFoundException ex){
-            System.out.println("Не удалось открыть файл " + ex.getMessage());
-            return false;
-        }
-        catch (UnsupportedEncodingException ex) {
-            System.out.println("Указана неизвестная кодировка " + ex.getMessage());
-            return false;
-        }
-        catch (IOException ex){
-            System.out.println("Возникла ошибка при копировании");
-            return false;
-        }
-        return true;
     }
 }
